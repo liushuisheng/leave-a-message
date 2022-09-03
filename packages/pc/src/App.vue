3943901cc9fc27f15957a0995ff6c8dd66650fed
刘水生 <liushuisheng@landray.com.cn>
2022-09-04 00:08:38 +0800

    feat: h5项目初始化

--- packages/pc/src/App.vue
+++ packages/pc/src/App.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div>给未来的自己留个言</div>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
+
+<script setup lang="ts">
+</script>
 
 <style>
 #app {
